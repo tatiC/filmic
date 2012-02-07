8592b2946326d4f9b99a8ff27c5c750d627e13ea
Tati Carvalho <tatigmc@yahoo.com.br>
2012-02-07 15:45:24 -0200

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filmic::Application.config.secret_token = '01b5bbe5d5802df98e1dc42247bc6114a3eb4bd39bd0ef170421a91900bb0805c7db7a1a33a20ad483335adb20b77e546abe2ab4fd483a341072c2456a988a48'
---
+++ film00.rb
@@ -0,0 +1,24 @@
+#encoding: utf-8
+
+class Filme
+  def initialize(filme, dica)
+    @filme = []
+    @filme << filme
+    @dica = dica
+    puts @filme                       
+    puts @dica
+    puts "\n"
+  end
+end
+
+puts "Iniciando..."
+puts "\n"
+Filme.new("Titanic", "Braços abertos.")
+Filme.new("Missão Impossível", "Ator principal come ectoplasma de bebê.")
+Filme.new("Thor", "Personagem chifrudo.")
+
+
+
+
+
+
